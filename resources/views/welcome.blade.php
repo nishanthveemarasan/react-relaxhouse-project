03a040c426e5115c2cce3afbdd3671519c303659
nishanthveema <iamnishanthveema@gmail.com>
2021-06-11 12:52:32 +0530

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -399,10 +399,9 @@
 </head>
 
 <body class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div id="example"></div>
-        <script src="/js/app.js"></script>
-    </div>
+    <div id="root"></div>
+    <script src="/js/app.js"></script>
+
 </body>
 
 </html>
